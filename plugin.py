d86a7d39bbf31e026facf11a455ae3fb2e92211d
Paulo Queiroz <pvaqueiroz@gmail.com>
2018-05-31 12:01:19 -0300

    Initial

---
+++ plugin.py
@@ -0,0 +1,5 @@
+import git
+import gi
+gi.require_version('WebKit', '3.0')
+
+from gi.repository import WebKit
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="pyide-git-plugin",
+    version="0.0.1",
+    description="Git integration plugin for PyIDE",
+)
