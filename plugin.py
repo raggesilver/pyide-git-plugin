94e8ccdc0a64674ef03f01b401a43bf8de55acb0
Paulo Queiroz <pvaqueiroz@gmail.com>
2018-06-02 17:45:11 -0300

    Inits ok

--- plugin.py
+++ plugin.py
@@ -1,5 +1,21 @@
-import git
-import gi
-gi.require_version('WebKit', '3.0')
 
-from gi.repository import WebKit
+import git, os
+
+from gi.repository import GObject, GLib, Gtk
+
+class Plugin(GObject.GObject):
+
+    def __init__(self, *args, **kwargs):
+
+        GObject.GObject.__init__(self)
+
+        if 'applicationWindow' in kwargs:
+            self.applicationWindow = kwargs['applicationWindow']
+
+    def do_activate(self):
+
+        self.applicationWindow.headerBar.pack_end(Gtk.Button('Git'))
+        self.applicationWindow.headerBar.show_all()
+
+    def say_ok(self, *args):
+        print("I'm ok")
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="pyide-git-plugin",
-    version="0.0.1",
-    description="Git integration plugin for PyIDE",
-)
